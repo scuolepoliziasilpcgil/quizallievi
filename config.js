5ea7478be387a89f44384a365b8472691d220f04
scuolepoliziasilpcgil <scuolepolizia.silp.cgil@gmail.com>
2026-06-13 00:37:58 +0200

    Create config.js with backend Apps Script URL
    Add configuration for quiz system with backend URL.

---
+++ config.js
@@ -0,0 +1,5 @@
+// Configurazione del sistema quiz allievi agenti
+//
+// Indirizzo del backend Apps Script.
+
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxsD7KImUy2Xa9egcfWmroGzoO1DEaHZwLt4Khocc4DOtAfGmSpPRrYUWufknSeV5fgoQ/exec";
